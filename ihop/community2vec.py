3b9219593b72d1944213259f66dea13ee7d6d732
Virginia Partridge <vcpartridge@gmail.com>
2022-01-07 19:29:56 -0500

    Cleanups to c2v model serialization

--- ihop/community2vec.py
+++ ihop/community2vec.py
@@ -12,8 +12,9 @@ Inputs:
 # TODO Considerations for cross validation
 """
 import csv
+import json
 import logging
-import pickle
+import os
 
 import gensim
 import pyspark.sql.functions as fn
@@ -43,12 +44,13 @@ class GensimCommunity2Vec:
     Determines the appropriate window-size and sets vocabulary according to the
     filtered subreddits.
     """
+    # When models are saved, what are the files named
+    MODEL_SAVE_NAME = "word2vec.pickle"
+    PARAM_SAVE_NAME = "parameters.json"
 
-    def __init__(self, spark, vocab_dict, contexts_path, vector_size=150, negative=20, sample=0, alpha=0.025, min_alpha=0.0001, seed=1, epochs=5, batch_words=10000, workers=3):
+    def __init__(self, vocab_dict, contexts_path, max_comments, num_users, vector_size=150, negative=20, sample=0, alpha=0.025, min_alpha=0.0001, seed=1, epochs=5, batch_words=10000, workers=3):
         """
         Instantiates a gensim Word2Vec model for Community2Vec
-        :param spark: SparkSession, needed for determining window size and number of users for learning rate decay. Most default values match gensim Word2Vec.
-
         :param vocab_dict: dict, str->int storing frequency counts of the vocab elements.
         :param contexts_path: Path to a text file storing the subreddits a user commented on, one user per line. Can be compressed as a bzip2 or gzip.
         :param vector_size: int, embedding size, passed to gensim Word2Vec
@@ -62,24 +64,20 @@ class GensimCommunity2Vec:
         :param workers: int, number of worker threads for training
         """
         self.contexts_path = contexts_path
-        self.context_df = spark.read.csv(contexts_path)
-        self.max_comments = self.__get_context_window()
-        self.num_users = self.__get_num_users()
+        self.max_comments = max_comments
+        self.num_users = num_users
         self.epochs = epochs
         self.w2v_model = gensim.models.word2vec.Word2Vec(vector_size=vector_size, min_count=0, window=self.max_comments, sg=1, hs=0, negative=negative, sample=sample, seed=seed, alpha=alpha, epochs=epochs, batch_words=batch_words, min_alpha=min_alpha, workers=workers)
         self.w2v_model.build_vocab_from_freq(vocab_dict)
 
-    def __get_context_window(self):
-        """Return the maximum number of comments a user had in the dataset.
-        This will become the window size for the Word2Vec model.
+    def get_params_as_dict(self):
+        """Returns dictionary of parameters that aren't stored in gensim's Word2Vec save()
         """
-        max_agg_df = self.context_df.select(fn.split("_c0", " ").alias("subreddit_list")).select("subreddit_list", fn.size("subreddit_list").alias("num_comments")).agg(fn.max("num_comments"))
-        return max_agg_df.head()[0]
-
-    def __get_num_users(self):
-        """Returns the number of users in the data, AKA number of contexts/sentences for W2V
-        """
-        return self.context_df.count()
+        return {"num_users": self.num_users,
+                "max_comments": self.max_comments,
+                "contexts_path": self.contexts_path,
+                "epochs": self.epochs
+                }
 
     def train(self, **kwargs):
         """Trains the word2vec model. Returns the result from gensim.
@@ -88,22 +86,55 @@ class GensimCommunity2Vec:
         train_result = self.w2v_model.train(gensim.models.word2vec.PathLineSentences(self.contexts_path), total_examples=self.num_users, epochs=self.epochs, **kwargs)
         return train_result
 
-    def save(self, save_path):
-        """Save the current model object to file
-        :param save_path: str or Path, file to save model to
+    def save(self, save_dir):
+        """Save the current model object with parameters in json and the word2vec model saved using the gensim save() method
+
+        :param save_dir: str, path of directory to save model and parameters
         """
-        with open(save_path, 'wb') as f:
-            pickle.dump(self, f)
+        if not os.path.exists(save_dir) and os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        w2v_path = os.path.join(save_dir, self.MODEL_SAVE_NAME)
+        self.w2v_model.save(w2v_path)
+        parameters_path = os.path.join(save_dir, self.PARAM_SAVE_NAME)
+
+        with open(parameters_path, 'w') as f:
+            json.dump(self.get_params_as_dict(), f)
 
     def save_vectors(self, save_path):
         """Save only the embeddings from this model as gensim KeyedVectors. These can't be used for further training of the Community2Vec model, but have smaller RAM footprint and are more efficient
         """
         self.w2v_model.wv.save(save_path)
 
-    def load(cls, load_path):
-        """Returns a GensimCommunity2Vec object that was pickled in the file.
-        :param load_path: str or Path, where to load the object from
+
+    @classmethod
+    def init_with_spark(cls, spark, vocab_dict, contexts_path, vector_size=150, negative=20, sample=0, alpha=0.025, min_alpha=0.0001, seed=1, epochs=5, batch_words=10000, workers=3):
+        """Instantiates a community2vec model using max_comments and num_users determined the contexts_path file using Spark.
+        :param vocab_dict: dict, str->int storing frequency counts of the vocab elements.
+        :param contexts_path: Path to a text file storing the subreddits a user commented on, one user per line. Can be compressed as a bzip2 or gzip.
+        :param vector_size: int, embedding size, passed to gensim Word2Vec
+        :param negative: int, how many 'noise words' should be drawn, passed to gensim Word2Vec
+        :param sample: float, the threshold for configuring which higher-frequency words are randomly downsampled, useful range is (0, 1e-5), passed to gensim Word2Vec, defaults to 0
+        :param alpha: float, initial learning rate, passed to gensim Word2Vec
+        :param min_alpha: float, minimum for learning rate decay
+        :param seed: int, randomSeed for initializing embeddings, passed to gensim Word2Vec
+        :param epochs: int, Number of iterations over the corpus, passed to gensim Word2Vec
+        :param batch_words: int, Target size (in words) for batches of examples passed to worker threads
+        :param workers: int, number of worker threads for training
         """
+        context_df = spark.read.csv(contexts_path)
+        max_comments = context_df.select(fn.split("_c0", " ").alias("subreddit_list")).select("subreddit_list", fn.size("subreddit_list").alias("num_comments")).agg(fn.max("num_comments")).head()[0]
+        num_users = context_df.count()
+
+        return cls(vocab_dict, contexts_path, max_comments, num_users,
+            vector_size, negative, sample, alpha, min_alpha,
+            seed, epochs, batch_words, workers)
+
+    @classmethod
+    def load(cls, load_dir):
+        """Returns a GensimCommunity2Vec object that was pickled in the file.
+        :param load_dir: str, directory to load the objects from
+        """
+        # TODO Finish method and tests for serializing/deserializing
         with open(load_path, 'rb') as f:
             return pickle.load(f)
 
--- tests/test_community2vec.py
+++ tests/test_community2vec.py
@@ -1,5 +1,7 @@
 import os
 
+import gensim
+
 import ihop.community2vec as c2v
 
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_files')
@@ -15,7 +17,7 @@ def test_get_vocab():
     assert vocab == expected_vocab
 
 def test_init_gensim_community2vec(spark):
-    c2v_model = c2v.GensimCommunity2Vec(spark, c2v.get_vocabulary(VOCAB_CSV), SAMPLE_SENTENCES, embedding_size = 25, epochs=2, batch_words=100, alpha=0.04)
+    c2v_model = c2v.GensimCommunity2Vec.init_with_spark(spark, c2v.get_vocabulary(VOCAB_CSV), SAMPLE_SENTENCES, vector_size = 25, epochs=2, batch_words=100, alpha=0.04)
     assert c2v_model.num_users == 4
     assert c2v_model.max_comments == 9
     assert c2v_model.epochs == 2
@@ -26,23 +28,23 @@ def test_init_gensim_community2vec(spark):
     assert len(c2v_model.w2v_model.wv["AskReddit"]) == 25
 
 
-def test_gensim_community2vec_train_no_errors(spark):
-    c2v_model = c2v.GensimCommunity2Vec(spark, c2v.get_vocabulary(VOCAB_CSV), SAMPLE_SENTENCES, embedding_size=25, epochs=2)
+def test_gensim_community2vec_train_no_errors():
+    c2v_model = c2v.GensimCommunity2Vec(c2v.get_vocabulary(VOCAB_CSV), SAMPLE_SENTENCES, 9, 4, vector_size=25, epochs=2)
     train_result = c2v_model.train()
-    assert type(train_result) == 'tuple'
+    assert type(train_result) == tuple
 
 
-def test_gensim_community2vec_compressed_sentences(spark):
-    c2v_model = c2v.GensimCommunity2Vec(spark, {"dndnext":1, "NBA2k":1}, SAMPLE_COMPRESSED, epochs=2)
+def test_gensim_community2vec_compressed_sentences():
+    c2v_model = c2v.GensimCommunity2Vec({"dndnext":1, "NBA2k":1}, SAMPLE_COMPRESSED, 1, 2, epochs=2)
     assert c2v_model.num_users == 2
     assert c2v_model.max_comments == 1
     train_result = c2v_model.train()
-    assert type(train_result) == 'tuple'
+    assert type(train_result) == tuple
 
 
-def test_save_load_trained_community2vec_model(spark, tmpdir):
-    save_path = tmpdir.join("model.pickle")
-    c2v_model = c2v.GensimCommunity2Vec(spark, c2v.get_vocabulary(VOCAB_CSV), SAMPLE_SENTENCES, epochs=2, alpha=0.05)
+def test_save_load_trained_community2vec_model(tmp_path):
+    save_path = str(tmp_path)
+    c2v_model = c2v.GensimCommunity2Vec(c2v.get_vocabulary(VOCAB_CSV), SAMPLE_SENTENCES, 9, 4, epochs=2, alpha=0.05)
     c2v_model.train()
     vector = c2v_model.w2v_model.wv['AskReddit']
     c2v_model.save(save_path)
@@ -55,5 +57,15 @@ def test_save_load_trained_community2vec_model(spark, tmpdir):
     assert loaded_model.w2v_model.wv["AskReddit"] == vector
 
 
-def test_save_vectors(spark, tmpdir):
-    pass
+def test_save_vectors(tmp_path):
+    save_path = str(tmp_path /"vectors.gz")
+    c2v_model = c2v.GensimCommunity2Vec(c2v.get_vocabulary(VOCAB_CSV), SAMPLE_SENTENCES,  9, 4, epochs=2, alpha=0.05, vector_size=25)
+    random_vector = c2v_model.w2v_model.wv["AskReddit"]
+
+    c2v_model.save_vectors(save_path)
+
+    loaded_vectors = gensim.models.KeyedVectors.load(save_path)
+
+    assert loaded_vectors.vector_size == 25
+    assert len(loaded_vectors.key_to_index) == 10
+    assert loaded_vectors["AskReddit"] == random_vector
