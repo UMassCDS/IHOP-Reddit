461b043ce00ff1556b622137343a7133baaaf3ea
Virginia Partridge <vcpartridge@gmail.com>
2022-02-03 18:53:01 +0000

    better slurm parameters

--- scripts/hyperparam_tune_c2v_slurm.sh
+++ scripts/hyperparam_tune_c2v_slurm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH -c 24
-#SBATCH --mem=8196
+#SBATCH -c 20
+#SBATCH -p cpu-long
+#SBATCH --mem=12000
 #SBATCH -t 72:00:00
-#SBATCH -s c2v-hyperparam-slurm-%j.out
+#SBATCH -o c2v-hyperparam-slurm-RC_2021-06_0percentTopUsersExcluded_01282022.out
 
-DATA_ROOT="data/community2vec/sample_data"
-#DATA_ROOT="data/community2vecs3/RC_2021-06_0percentTopUsersExcluded_01282022"
+#DATA_ROOT="data/community2vec/sample_data"
+DATA_ROOT="data/community2vecs3/RC_2021-06_0percentTopUsersExcluded_01282022"
 #DATA_ROOT="data/community2vecs3/RC_2021-06_2percentTopUsersExcluded_01282022"
 #DATA_ROOT="data/community2vecs3/RC_2021-06_5percentTopUsersExcluded_01282022"
 #DATA_ROOT="data/community2vecs3/RC_2021-06_10percentTopUsersExcluded_01282022"
@@ -21,6 +22,6 @@ conda activate ihop
 pip install -r requirements.txt
 pip install .
 
-python -m ihop.community2vec --contexts $CONTEXTS --vocab_csv $VOCAB --output_dir $MODEL_DIR --param_grid '{"alpha": [0.05, 0.01], "vector_size":[150], "sample":[0.005, 0.05], "negative":[20,40]}' --workers 24
+python -m ihop.community2vec --contexts $CONTEXTS --vocab_csv $VOCAB --output_dir $MODEL_DIR --param_grid '{"alpha": [0.05, 0.01], "vector_size":[150], "sample":[0.005, 0.05], "negative":[20,40]}' --workers 40
 
 module purge
