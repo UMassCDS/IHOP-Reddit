8fea06ea08fb4c6e0b0551e2e30fb81e5575d472
Virginia Partridge <vcpartridge@gmail.com>
2022-04-01 10:41:18 -0400

    Visualization of TSNE projection on UI

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 """Visualize subreddit clusters
 Run using `python app.py` and visit http://127.0.0.1:8050
+
+# TODO vector models should be configurable
 """
 import argparse
 import logging
@@ -9,12 +11,37 @@ import plotly.express as px
 import pandas as pd
 
 import ihop.utils
+import ihop.community2vec
 
 logger = logging.getLogger(__name__)
 
 app = Dash(__name__)
 
-app.layout = html.Div(children=[html.H1(children="Hello Dash")])
+# TODO Config and select from multiple models
+c2v = ihop.community2vec.GensimCommunity2Vec.load(
+    "data/community2vec/RC_2021-05_5percentTopUsersExcluded_02142022/models/alpha0.05_negative10_sample0.005_vectorSize100"
+)
+
+
+def get_cluster_visualization(vector_model, subreddits=None, clusters=None):
+    """Build the plotly visualization for a model
+
+    :param vector_model: Gensim community to vec for clusters
+    :param subreddits: list of subreddits to filter clusters from
+    :param cluster: list of cluster ids to filter visualization
+    """
+    df, _ = vector_model.get_tsne_dataframe()
+    return px.scatter(
+        df, x="tsne_x", y="tsne_y", text="subreddit", hover_data=["subreddit"],
+    )
+
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Hello Dash"),
+        dcc.Graph(id="test_c2v_clusters", figure=get_cluster_visualization(c2v)),
+    ]
+)
 
 
 parser = argparse.ArgumentParser(
